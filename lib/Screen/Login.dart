0254fcd2c70ec44c05b72ffce393259088289e37
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2022-10-29 19:04:54 +0300

    first commit

---
+++ lib/Screen/Login.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:usercolor/Screen/BackgrounColor.dart';
+import 'package:usercolor/Screen/SignUp.dart';
+
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+        const Center(
+          child: Text('welcom your app'),
+        ),
+        const Divider(height: 25),
+        const Text("email"),
+        TextField(
+          controller: emailController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'email',
+          ),
+        ),
+        const Text("password"),
+        TextField(
+          controller: passwordController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'password',
+          ),
+        ),
+        const Divider(
+          height: 25,
+        ),
+        TextButton(
+            onPressed: () async {
+              try {
+                var login = FirebaseAuth.instance;
+                UserCredential userCredential =
+                    await login.signInWithEmailAndPassword(
+                        email: emailController.text,
+                        password: passwordController.text);
+                // ignore: use_build_context_synchronously
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => const BackgroundColor()));
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(e.toString()),
+                ));
+              }
+            },
+            child: const Text('Sign in')),
+            const Divider(height: 25),
+            TextButton(
+              onPressed:(){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => const SignUp()));
+              
+              },
+              child: const Text('Sign up'),
+            )
+      ]),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:usercolor/Screen/Login.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+// Ideal time to initialize
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  runApp(const MaterialApp(home: MyApp()));
+//...
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Login();
+  }
+}
